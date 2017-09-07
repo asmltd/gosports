78d204bf23a7286f40acf7e8c1d745bb48b31f13
hariharaselvam <hariharaselvam8@gmail.com>
2017-09-07 15:39:42 +0530

    mysql database

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gosports',
+        'USER': 'gosports',
+        'PASSWORD': 'g05ports',
+        'HOST': '',  # Or an IP Address that your DB is hosted on ( default localhost )
+        'PORT': '',
     }
 }
 
