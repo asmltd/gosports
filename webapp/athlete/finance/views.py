3351a8980ec81cfe65cc6240c0a98310acab4d67
mohanraj1992 <kctcsmohan@gmail.com>
2017-11-02 19:20:39 +0530

    adding code to delete the file when any finance item is deleted via api

--- webapp/athlete/finance/views.py
+++ webapp/athlete/finance/views.py
@@ -4,15 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, \
-    RetrieveDestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import FinanceSerializer
 from .models import Athlete_Finance
-
+from webapp.settings import *
 
 class FinanceCreateAPIView(CreateAPIView):
     queryset = Athlete_Finance.objects.all()
@@ -50,7 +51,6 @@ class FinanceDetailAPIView(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
 
 
-
 class FinanceUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Athlete_Finance.objects.all()
     serializer_class = FinanceSerializer
@@ -63,3 +63,12 @@ class FinanceDeleteAPIView(RetrieveDestroyAPIView):
     serializer_class = FinanceSerializer
     authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
+
+    def delete(self, request, *args, **kwargs):
+        import os
+        instance = self.get_object()
+        filepath = instance.finance_proof
+        filepath_whole = str(MEDIA_ROOT)+ "/"+ str(filepath)
+        os.remove(filepath_whole)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
